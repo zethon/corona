88ab61246fdedc11413d8a85cf466c8b1196ea5b
Adalid Claure <adalid.claure@rsa.com>
2020-03-27 07:15:09 -0400

    tracking world and us numbers

--- tracker.pl
+++ tracker.pl
@@ -7,23 +7,48 @@ use POSIX qw(strftime);
 
 my $ua = LWP::UserAgent->new();
 $ua->agent("Mozilla/5.0"); 
-my $response = $ua->get("https://www.worldometers.info/coronavirus/country/us/");
-my $content = $response->decoded_content();
 
-my $total = "";
-my $deaths = "";
+# GET THE WORLD STATS
+my $worldtotal = "";
+my $worlddeaths = "";
 
-if ($content =~ /<h1>Coronavirus Cases:<\/h1>\n\s+.*\n\s+<span.*?>([0-9,\s]+)<\/span>/)
 {
-    $total = $1;
-    $total =~ s/[,\s]//g;
+    my $response = $ua->get("https://www.worldometers.info/coronavirus/");
+    my $content = $response->decoded_content();
+
+    if ($content =~ /<h1>Coronavirus Cases:<\/h1>\n\s+.*\n\s+<span.*?>([0-9,\s]+)<\/span>/)
+    {
+        $worldtotal = $1;
+        $worldtotal =~ s/[,\s]//g;
+    }
+
+    if ($content =~ /<h1>Deaths:<\/h1>\n\s+.*\n\s+<span.*?>([0-9,]+)<\/span>/)
+    {
+        $worlddeaths = $1;
+        $worlddeaths =~ s/,//g;
+    }
 }
 
-if ($content =~ /<h1>Deaths:<\/h1>\n\s+.*\n\s+<span>([0-9,]+)<\/span>/)
+# GET THE US STATS
+my $ustotal = "";
+my $usdeaths = "";
+
 {
-    $deaths = $1;
-    $deaths =~ s/,//g;
+    my $response = $ua->get("https://www.worldometers.info/coronavirus/country/us/");
+    my $content = $response->decoded_content();
+
+    if ($content =~ /<h1>Coronavirus Cases:<\/h1>\n\s+.*\n\s+<span.*?>([0-9,\s]+)<\/span>/)
+    {
+        $ustotal = $1;
+        $ustotal =~ s/[,\s]//g;
+    }
+
+    if ($content =~ /<h1>Deaths:<\/h1>\n\s+.*\n\s+<span>([0-9,]+)<\/span>/)
+    {
+        $usdeaths = $1;
+        $usdeaths =~ s/,//g;
+    }
 }
 
 my $datestring = strftime("%Y-%m-%d %H:%M:%S", gmtime());
-print ("$datestring,$total,$deaths\n");
+print ("$datestring,$worldtotal,$worlddeaths,$ustotal,$usdeaths\n");
